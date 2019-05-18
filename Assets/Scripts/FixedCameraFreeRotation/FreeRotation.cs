3d9fad45c40141511b5ed72884a061d2c7522501
SmkMth <dandavidnoon@gmail.com>
2019-05-18 13:18:59 +0100

    updated to work with controller
    somwhat - if its buggy dont @ me

--- Assets/Scripts/FixedCameraFreeRotation/FreeRotation.cs
+++ Assets/Scripts/FixedCameraFreeRotation/FreeRotation.cs
@@ -7,7 +7,7 @@ public class FreeRotation : MonoBehaviour
     public float StartRotateSpeed;
     public float MaxRotateSpeed;
     public float Step;
-
+    public bool usingMouse;
     
 
     private float rot;
@@ -22,13 +22,31 @@ public class FreeRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-        if (Input.GetAxis("Mouse X") != 0)
+       if (usingMouse)
         {
-            rot = Mathf.Lerp(StartRotateSpeed, MaxRotateSpeed, Step * Time.deltaTime);
+            if (Input.GetAxis("Mouse X") != 0)
+            {
+                rot = Mathf.Lerp(StartRotateSpeed, MaxRotateSpeed, Step * Time.deltaTime);
+            }
+
+            Debug.Log(Input.GetAxis("RightStickHorizontal"));
+            transform.Rotate(0, rot * Input.GetAxis("Mouse X"), 0);
+
+        }
+        else
+        {
+
+            //Vector3 lookDirection = new Vector3(Input.GetAxis("RightStickHorizontal"), 0, Input.GetAxis("RightStickVertical"));
+
+            float RsHorizontal = Input.GetAxis("RightStickHorizontal");
+            float RsVertical = Input.GetAxis("RightStickVertical");
+            Vector3 lookDirection = new Vector3(RsVertical, 0.0f, RsHorizontal);
+
+            transform.rotation = Quaternion.Euler(lookDirection);
+            transform.LookAt(transform.position + lookDirection, Vector3.up);
+
         }
        
 
-        transform.Rotate(0, rot * Input.GetAxis("Mouse X"), 0);
     }
 }
